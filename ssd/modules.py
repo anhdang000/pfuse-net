47b26877c87d491e8749eac91267ee313c1a2327
anhdang000 <danglinh.anh000@gmail.com>
2021-06-20 11:43:21 +0700

    Change dim=1 to dim=2 as channel-wise concat

--- ssd/modules.py
+++ ssd/modules.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch
 
+
 #Add a class for Concat module here:
 class Concatenate(nn.Module):
     def __init__(self, channels):
@@ -8,9 +9,10 @@ class Concatenate(nn.Module):
         self.conv = nn.Conv2d(2*channels, channels, kernel_size=1, bias=False)
     def forward(self, x):
         rgb_x, lp_x = x[0], x[1]
-        result = torch.cat((rgb_x, lp_x), dim=1)
+        result = torch.cat((rgb_x, lp_x), dim=2)
         result = self.conv(result)
         return [result, lp_x]
+
 
 class Exchange(nn.Module):
     def __init__(self):
@@ -25,6 +27,27 @@ class Exchange(nn.Module):
         x2[:, bn2 < bn_threshold] = x[0][:, bn2 < bn_threshold]
         return [x1, x2]
 
+
+class L2Norm(nn.Module):
+    def __init__(self, input_channels=512, scale=20):
+        super(L2Norm, self).__init__()
+        self.weight = nn.Parameter(torch.Tensor(input_channels))
+        self.scale = scale
+        self.reset_parameters()
+        self.eps = 1e-10
+    
+    def reset_parameters(self):
+        nn.init.constant_(self.weight, self.scale)
+    
+    def forward(self, x):
+        # x.size() = (batch_size, chanenel, height, width)
+        # L2Norm
+        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt() + self.eps
+        x = torch.div(x, norm)
+        #weight.size() = (512) -> (1,512,1,1)
+        weights = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x)
+
+        return weights*x
 
 
 class ModuleParallel(nn.Module):
@@ -44,3 +67,4 @@ class BatchNorm2dParallel(nn.Module):
 
     def forward(self, x_parallel):
         return [getattr(self, 'bn_' + str(i))(x) for i, x in enumerate(x_parallel)]
+
