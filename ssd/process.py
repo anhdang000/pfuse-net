d741be0a343c849a66c48d8c76e9bee686b6d226
sonnguyentruong129 <sonhai12092000@gmail.com>
2021-06-23 04:38:44 +0700

    Add mAP

--- ssd/process.py
+++ ssd/process.py
@@ -2,10 +2,10 @@
 @author: Viet Nguyen <nhviet1009@gmail.com>
 """
 import numpy as np
-from tqdm.autonotebook import tqdm
+import tqdm
 import torch
-from pycocotools.cocoeval import COCOeval
-from apex import amp
+# from pycocotools.cocoeval import COCOeval
+# from apex import amp
 
 
 def train(model, train_loader, epoch, writer, criterion, optimizer, scheduler, is_amp):
@@ -41,9 +41,11 @@ def train(model, train_loader, epoch, writer, criterion, optimizer, scheduler, i
 def evaluate(model, test_loader, epoch, writer, encoder, nms_threshold):
     model.eval()
     detections = []
+    true_boxes = []
     category_ids = test_loader.dataset.coco.getCatIds()
-    for nbatch, (img, lp_img, img_id, img_size, _, _) in enumerate(test_loader):
+    for nbatch, (img, lp_img, img_id, img_size, img_box, img_label) in enumerate(test_loader):
         print("Parsing batch: {}/{}".format(nbatch, len(test_loader)), end="\r")
+        true_boxes.append([img_id, img_label, *img_box])
         if torch.cuda.is_available():
             img = img.cuda()
             lp_img = lp_img.cuda()
@@ -64,15 +66,147 @@ def evaluate(model, test_loader, epoch, writer, encoder, nms_threshold):
                 height, width = img_size[idx]
                 loc, label, prob = [r.cpu().numpy() for r in result]
                 for loc_, label_, prob_ in zip(loc, label, prob):
-                    detections.append([img_id[idx], loc_[0] * width, loc_[1] * height, (loc_[2] - loc_[0]) * width,
-                                       (loc_[3] - loc_[1]) * height, prob_,
-                                       category_ids[label_ - 1]])
+                    detections.append([img_id[idx], category_ids[label_ - 1], prob_, loc_[0] * width, loc_[1] * height, (loc_[2] - loc_[0]) * width,
+                                       (loc_[3] - loc_[1]) * height])
 
     detections = np.array(detections, dtype=np.float32)
 
-    coco_eval = COCOeval(test_loader.dataset.coco, test_loader.dataset.coco.loadRes(detections), iouType="bbox")
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()
+    map = mean_average_precision(detections, true_boxes)
+    print('mAP: ', map)
 
-    writer.add_scalar("Test/mAP", coco_eval.stats[0], epoch)
+    # coco_eval = COCOeval(test_loader.dataset.coco, test_loader.dataset.coco.loadRes(detections), iouType="bbox")
+    # coco_eval.evaluate()
+    # coco_eval.accumulate()
+    # coco_eval.summarize()
+
+    # writer.add_scalar("Test/mAP", coco_eval.stats[0], epoch)
+def intersection_over_union(boxes_preds, boxes_labels):
+    """
+    Calculates intersection over union
+    Parameters:
+        boxes_preds (tensor): Predictions of Bounding Boxes (BATCH_SIZE, 4)
+        boxes_labels (tensor): Correct Labels of Boxes (BATCH_SIZE, 4)
+        box_format (str): midpoint/corners, if boxes (x,y,w,h) or (x1,y1,x2,y2)
+    Returns:
+        tensor: Intersection over union for all examples
+    """
+
+    # Slicing idx:idx+1 in order to keep tensor dimensionality
+    # Doing ... in indexing if there would be additional dimensions
+    # Like for Yolo algorithm which would have (N, S, S, 4) in shape
+    
+    box1_x1 = boxes_preds[..., 0:1]
+    box1_y1 = boxes_preds[..., 1:2]
+    box1_x2 = boxes_preds[..., 2:3]
+    box1_y2 = boxes_preds[..., 3:4]
+    box2_x1 = boxes_labels[..., 0:1]
+    box2_y1 = boxes_labels[..., 1:2]
+    box2_x2 = boxes_labels[..., 2:3]
+    box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+def mean_average_precision(
+    pred_boxes, true_boxes, iou_threshold=0.5, num_classes=7
+):
+    """
+    Calculates mean average precision 
+    Parameters:
+        pred_boxes (list): list of lists containing all bboxes with each bboxes
+        specified as [train_idx, class_prediction, prob_score, x1, y1, x2, y2]
+        true_boxes (list): Similar as pred_boxes except all the correct ones 
+        iou_threshold (float): threshold where predicted bboxes is correct
+        box_format (str): "midpoint" or "corners" used to specify bboxes
+        num_classes (int): number of classes
+    Returns:
+        float: mAP value across all classes given a specific IoU threshold 
+    """
+
+    average_precisions = []
+
+    epsilon = 1e-6
+
+    for c in range(num_classes):
+        detections = []
+        ground_truths = []
+
+        # Go through all predictions and targets,
+        # and only add the ones that belong to the
+        # current class c
+        for detection in pred_boxes:
+            if detection[1] == c:
+                detections.append(detection)
+
+        for true_box in true_boxes:
+            if true_box[1] == c:
+                ground_truths.append(true_box)
+
+        # find the amount of bboxes for each training example
+        # Counter here finds how many ground truth bboxes we get
+        # for each training example, so let's say img 0 has 3,
+        # img 1 has 5 then we will obtain a dictionary with:
+        # amount_bboxes = {0:3, 1:5}
+        amount_bboxes = Counter([gt[0] for gt in ground_truths])
+
+        # We then go through each key, val in this dictionary
+        # and convert to the following (w.r.t same example):
+        # ammount_bboxes = {0:torch.tensor[0,0,0], 1:torch.tensor[0,0,0,0,0]}
+        for key, val in amount_bboxes.items():
+            amount_bboxes[key] = torch.zeros(val)
+
+        detections.sort(key=lambda x: x[2], reverse=True)
+        TP = torch.zeros((len(detections)))
+        FP = torch.zeros((len(detections)))
+        total_true_bboxes = len(ground_truths)
+        
+        if total_true_bboxes == 0:
+            continue
+
+        for detection_idx, detection in enumerate(detections):
+            # Only take out the ground_truths that have the same
+            # training idx as detection
+            ground_truth_img = [
+                bbox for bbox in ground_truths if bbox[0] == detection[0]
+            ]
+
+            num_gts = len(ground_truth_img)
+            best_iou = 0
+
+            for idx, gt in enumerate(ground_truth_img):
+                iou = intersection_over_union(
+                    torch.tensor(detection[3:]),
+                    torch.tensor(gt[3:]),
+                    box_format=box_format,
+                )
+
+                if iou > best_iou:
+                    best_iou = iou
+                    best_gt_idx = idx
+
+            if best_iou > iou_threshold:
+                if amount_bboxes[detection[0]][best_gt_idx] == 0:
+                    TP[detection_idx] = 1
+                    amount_bboxes[detection[0]][best_gt_idx] = 1
+                else:
+                    FP[detection_idx] = 1
+            else:
+                FP[detection_idx] = 1
+
+        TP_cumsum = torch.cumsum(TP, dim=0)
+        FP_cumsum = torch.cumsum(FP, dim=0)
+        recalls = TP_cumsum / (total_true_bboxes + epsilon)
+        precisions = TP_cumsum / (TP_cumsum + FP_cumsum + epsilon)
+        precisions = torch.cat((torch.tensor([1]), precisions))
+        recalls = torch.cat((torch.tensor([0]), recalls))
+        average_precisions.append(torch.trapz(precisions, recalls))
+
+    return sum(average_precisions) / len(average_precisions)
